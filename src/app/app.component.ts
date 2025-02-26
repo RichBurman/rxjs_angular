767b2b6028e0a046d715da6bc6cc96af625fdf4c
Richard Burman <richard.burman@harperjames.co.uk>
2025-02-26 11:52:47 +0000

    Add click event handler and signal for click count tracking

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, DestroyRef, inject, OnInit } from '@angular/core';
+import { Component, computed, DestroyRef, effect, inject, OnInit, signal } from '@angular/core';
 
 import { interval, map } from 'rxjs';
 
@@ -8,17 +8,32 @@ import { interval, map } from 'rxjs';
   templateUrl: './app.component.html',
 })
 export class AppComponent implements OnInit {
-
+  clickCount = signal(0);
+  // interval = signal(0);
+  // doubleInterval = computed(() => this.interval() * 2);
   private destroyRef = inject(DestroyRef);
-  ngOnInit(): void {
-    const subscription = interval(1000).pipe(
-      map((val) => val * 2)
-    ).subscribe({
-      next: (val) => console.log(val),
-    });
 
-    this.destroyRef.onDestroy(() => {
-      subscription.unsubscribe();
+  constructor() {
+    effect(() => {
+      console.log(`Clicked button ${this.clickCount} times`);
     });
+  }
+  ngOnInit(): void {
+    // setInterval(() => {
+    //   this.interval.update(prevCount => prevCount + 1);
+    // }, 1000);
+    // const subscription = interval(1000).pipe(
+    //   map((val) => val * 2)
+    // ).subscribe({
+    //   next: (val) => console.log(val),
+    // });
+
+    // this.destroyRef.onDestroy(() => {
+    //   subscription.unsubscribe();
+    // });
+  }
+
+  onClick() {
+    this.clickCount.update(prevCount => prevCount + 1);
   }
 }
