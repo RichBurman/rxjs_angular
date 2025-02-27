700f98ad18bd10242093d3f7b9512a37523eba06
Richard Burman <richard.burman@harperjames.co.uk>
2025-02-27 10:59:26 +0000

    Add custom interval observable to emit values and log completion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, computed, DestroyRef, effect, inject, OnInit, signal } from '@angular/core';
 import { toObservable, toSignal } from '@angular/core/rxjs-interop';
 
-import { interval, map } from 'rxjs';
+import { interval, map, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -13,6 +13,19 @@ export class AppComponent implements OnInit {
   clickCount$ = toObservable(this.clickCount);
   interval$ = interval(1000);
   intervalSignal = toSignal(this.interval$, { initialValue: 0 });
+  customInterval$ = new Observable((subscriber) => {
+    let timesExecuted = 0;
+    const interval = setInterval(() => {
+      if (timesExecuted > 5) {
+        clearInterval(interval);
+        subscriber.complete();
+        return;
+      }
+      console.log('Emitting new value...');
+      subscriber.next({message: 'New value'});
+      timesExecuted++;
+    }, 2000)
+  });
   // interval = signal(0);
   // doubleInterval = computed(() => this.interval() * 2);
   private destroyRef = inject(DestroyRef);
@@ -37,6 +50,10 @@ export class AppComponent implements OnInit {
     // this.destroyRef.onDestroy(() => {
     //   subscription.unsubscribe();
     // });
+    this.customInterval$.subscribe({
+      next: (val) => console.log(val),
+      complete: () => console.log('Completed')
+    });
     const subscription = this.clickCount$.subscribe({
       next: (val) =>
         console.log(`Clicked button ${this.clickCount} times`)
